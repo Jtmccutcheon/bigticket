398e1c807e4a2f6b60ffb0ca8c4a96693646e30e
unknown <jt.mccutcheon@live.com>
2020-11-11 12:12:31 -0500

    set up skaffold and ingress locally began auth routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { json } from 'body-parser';
+import { currentUserRouter } from './routes/currentuser';
 
 const app = express();
 app.use(json());
+app.use(currentUserRouter);
 
 app.listen(3000, () => {
-	console.log(`listening on port 3000`);
+	console.log(`listening on port 3000, busterssssss`);
 });
