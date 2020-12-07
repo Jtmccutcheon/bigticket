c7a9e822382f377dad869fade1c2deef042dee24
unknown <jt.mccutcheon@live.com>
2020-12-06 20:24:30 -0500

    add currentuser route

--- auth/src/routes/currentuser.ts
+++ auth/src/routes/currentuser.ts
@@ -1,8 +1,21 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-	res.send('hi there');
+	// if the session doesnt exist with our token on it
+	if (!req.session?.jwt) {
+		return res.send({ currentUser: null });
+	}
+
+	try {
+		// verify takes in two arguments the token from the user and our secret
+		const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+		res.send({ currentUser: payload });
+	} catch (error) {
+		res.send({ currentUser: null });
+	}
 });
 
 export { router as currentUserRouter };
