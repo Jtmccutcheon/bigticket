b5a45d23899c0c24151a0c7715ca2915059ccd51
unknown <jt.mccutcheon@live.com>
2020-12-06 20:07:25 -0500

    finish sign in

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { validateRequest } from '../middlewares/validateRequest';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/badRequestError';
+import { Password } from '../services/password';
+import jwt from 'jsonwebtoken';
+
 const router = express.Router();
 
 router.post(
@@ -13,7 +18,39 @@ router.post(
 			.withMessage('You must supply a password'),
 	],
 	validateRequest,
-	(req: Request, res: Response) => {},
+	async (req: Request, res: Response) => {
+		const { email, password } = req.body;
+		const existingUser = await User.findOne({ email });
+
+		if (!existingUser) {
+			throw new BadRequestError('Invalid credentials');
+		}
+
+		const PasswordsMatch = await Password.compare(
+			existingUser.password,
+			password,
+		);
+
+		if (!PasswordsMatch) {
+			throw new BadRequestError('Invalid credentials');
+		}
+
+		// generate jwt
+		const userJwt = jwt.sign(
+			{
+				id: existingUser.id,
+				email: existingUser.email,
+			},
+			process.env.JWT_KEY!, // ! means typescript ignore
+		);
+
+		// store on the session obj
+		req.session = {
+			jwt: userJwt,
+		};
+
+		res.status(200).send(existingUser);
+	},
 );
 
 export { router as signInRouter };
