af7ccc2372be948aacaf4700c68152bce49954fc
unknown <jt.mccutcheon@live.com>
2020-12-06 20:59:11 -0500

    add requireAuth middleware and new custom error

--- auth/src/routes/currentuser.ts
+++ auth/src/routes/currentuser.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
 import { currentUser } from '../middlewares/currentUser';
+import { requireAuth } from '../middlewares/requireAuth';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', currentUser, (req, res) => {
+router.get('/api/users/currentuser', currentUser, requireAuth, (req, res) => {
 	res.send({ currentUser: req.currentUser || null }); // null if we dont pass current User middleware
 });
 
