a30a9f3ef5e9da040e0e6c34611afd6c5576bac3
unknown <jt.mccutcheon@live.com>
2020-12-14 16:30:31 -0500

    set up inital props

--- client/pages/index.js
+++ client/pages/index.js
@@ -10,7 +10,26 @@ const LandingPage = ({ currentUser }) => {
 // we have to do that in this function
 // and we will have access to it as props normally
 LandingPage.getInitialProps = async () => {
-	const response = await axios.get('/api/users/currentuser');
-	return response.data;
+	if (typeof window === 'undefined') {
+		// we are on the server
+		// requests made to full endpoint
+
+		const { data } = await axios.get(
+			// 'http://auth-srv:3000/api/users/currentuser', // could also use this
+			// ingress-nginx-controller comes from this command kubectl get services -n ingress-nginx
+
+			'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+			{
+				headers: { Host: 'ticketing.dev' },
+			},
+		);
+
+		return data;
+	} else {
+		// we are on the browser
+		// requests can be short hand
+		const { data } = await axios.get('/api/users/currentuser');
+		return data;
+	}
 };
 export default LandingPage;
